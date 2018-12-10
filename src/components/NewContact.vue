5f294dec3ed9fe6bb2a7330d9a1da779e4a18c6a
unknown <zhoushuyuan@360buyad.local>
2018-12-10 20:18:32 +0800

    add some code

--- src/components/NewContact.vue
+++ src/components/NewContact.vue
@@ -1,31 +1,33 @@
 <template>
   <el-row>
-    姓名：{{info.name}}
-    <el-input v-model="info.name" placeholder="请输入姓名"></el-input>
-    年龄：{{info.age}}
-    <el-input v-model="info.age" placeholder="请输入年龄"></el-input>
-    性别：{{info.sex}}
-    <el-select v-model="info.sex" placeholder="请选择">
-      <el-option v-for="item in options" :key="item" :value="item"></el-option><!-- 这里的key官方推荐在v-for时使用，不然会警告，但不影响使用 -->
-    </el-select>
-    <el-button @click="create">创建</el-button>
-    <template>
-      <el-table :data="tabledata" align="left">
-        <el-table-column prop="name" label="姓名"></el-table-column>
-        <el-table-column prop="age" label="年龄"></el-table-column>
-        <el-table-column prop="sex" label="性别"></el-table-column>
-        <el-table-column label="操作">
-          <template slot-scope="a">
-            <el-button size="mini" type="danger" @click="del(a.$index)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-    </template>
-
+    <el-col :xs="24" :sm="18" :md="14" :lg="10" id="main">
+      <label>姓名：</label>
+      <el-input v-model="info.name" placeholder="请输入姓名"></el-input>
+      <label>年龄：</label>
+      <el-input v-model="info.age" placeholder="请输入年龄"></el-input>
+      <label>性别：</label>
+      <el-select v-model="info.sex" placeholder="请选择">
+        <el-option v-for="item in options" :key="item" :value="item"></el-option><!-- 这里的key官方推荐在v-for时使用，不然会警告，但不影响使用 -->
+      </el-select>
+      <el-button class="btn-auto" @click="create" type="success">创建</el-button>
+      <template>
+        <el-table :data="tabledata" align="left">
+          <el-table-column prop="name" label="姓名"></el-table-column>
+          <el-table-column prop="age" label="年龄"></el-table-column>
+          <el-table-column prop="sex" label="性别"></el-table-column>
+          <el-table-column label="操作">
+            <template slot-scope="a">
+              <el-button size="mini" type="danger" @click="del(a.$index)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </template>
+      </el-col>
   </el-row>
 </template>
 
 <script>
+import Storage from '../store/store' // 新添加，把刚写的localStorage导入
 export default {
   name: 'NewContact',
   data () {
@@ -33,30 +35,47 @@ export default {
       info: {
         name: '',
         age: null,
-        sex: '' },
+        sex: ''
+      },
       options: [
         '女', '男', '保密'
       ],
-      tabledata: [
-        {name: 'Leo', age: 12, sex: 'man'},
-        {name: 'Lei', age: 22, sex: 'women'},
-        {name: 'Lii', age: 65, sex: 'men'}
-      ]
+      tabledata: Storage.fetch() // 把之前的删除，写入这个获取数据的方法
     }
   },
+
   methods: {
     create () {
-      this.tabledata.push(this.info) // 给tabledata添加一个对象（之前我们创建的info）
-      this.info = {name: '', age: null, sex: ''}// 点击创建后，让option还原，而不是停留在所选的项
+      this.tabledata.push(this.info)
+      this.info = {name: '', age: null, sex: ''}
     },
     del (index) {
-      this.tabledata.splice(index, 1)// 删除点击的对象，index是lot-scope="a" a.$index传过来的
+      this.tabledata.splice(index, 1)
+    }
+  },
+
+  watch: { // 新添加，watch是vue的监听，一旦监听对象有变化就会执行相应操作
+    tabledata: { // 新添加，被监听的对象
+      handler (items) { Storage.save(items) }, // 新添加，监听对象变化后所做的操作，一个函数，保存数据
+      deep: true // 深度监听，避免数据的嵌套层数太多，我们要使用深度监听，这样即使数据层级过多监听不到
     }
   }
-
 }
 
 </script>
 <style>
-
+    #main{
+        float: none;
+        margin: 0 auto;
+    }
+    .el-input{
+      padding-bottom: 5px;
+    }
+    .el-select {
+        display: block;
+    }
+    .btn-auto{
+        width: 100%;
+        margin-top: 12px;
+    }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,9 @@
-const STORAGE_KEY = 'table-vuejs'   //名字随便起
-export default {
-  //向往输出，以便组件接收
-  fetch() {
-    //我们定义的获取数据的方法
+const STORAGE_KEY = 'tabale-vuejs'
+export default {                                                                 //向往输出，以便组件接收
+  fetch() {                                                                     //我们定义的获取数据的方法
     return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
   },
-  save(items) {
-      // 我们定义的保存数据的方法
-      window.localStorage.getItem(STORAGE_KEY, JSON.stringify(items))
+  save(items) {                                                                 //我们定义的保存数据的方法
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
   }
 }
